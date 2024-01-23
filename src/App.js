743136f1377a32c78b7fca70f88eb6f280ab612d
leulaaa <leulaaa12@gmail.com>
2024-01-23 13:42:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
